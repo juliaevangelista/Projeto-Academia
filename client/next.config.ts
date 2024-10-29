5ab9984f610d361ca6ceaf0abb314ca87e9b5c53
viinDevloper <digitaltsx.vinicius@outlook.com>
2024-10-29 10:31:56 -0300

    build:  Configuration NextConfig add typedRoutes, typescript add, ignoreBuildErrors

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    typedRoutes: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
