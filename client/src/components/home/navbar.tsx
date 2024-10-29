f98aaab83c02ba5d28a606727b08fb58d37412a9
juliaevangelista <juliaevangelista70@gmail.com>
2024-10-29 11:18:47 -0300

    update Link

--- client/src/components/home/navbar.tsx
+++ client/src/components/home/navbar.tsx
@@ -1,29 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
   return (
     <div className="flex flex-row w-full items-center justify-center text-base">
-      <a href="#logo" className="mr-[100px] font-bold text-2xl">
+      <Link href="#logo" className="mr-[100px] font-bold text-2xl">
         Leo & Cia Academia
-      </a>
+      </Link>
       <nav className="flex justify-between items-center gap-[40px] h-[72px] px-8">
-        <a href="#" className="">
+        <Link href="#" className="">
           Inicio
-        </a>
-        <a href="#" className="">
+        </Link>
+        <Link href="#" className="">
           Sobre Nós
-        </a>
-        <a href="#" className="">
+        </Link>
+        <Link href="#" className="">
           Treinos Disponiveis
-        </a>
-        <a href="#" className="">
+        </Link>
+        <Link href="#" className="">
           Plano
-        </a>
-        <a href="#" className="">
+        </Link>
+        <Link href="#" className="">
           Contato
-        </a>
+        </Link>
       </nav>
-      <button className="bg-slate-400 rounded-lg p-1 ml-[121.5px]">Junte-se a nós</button>
+      <button className="bg-slate-400 rounded-lg p-1 ml-[121.5px]">
+        Junte-se a nós
+      </button>
     </div>
   );
 };
